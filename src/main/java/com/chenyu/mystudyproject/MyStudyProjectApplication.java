fe0ea58acd6af68c2e999fe7ba8537e701f6fce0
chenyu <yu.chen@tioad.com>
2020-10-23 15:50:16 +0800

    初始化

---
+++ src/main/java/com/chenyu/mystudyproject/MyStudyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.chenyu.mystudyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStudyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStudyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chenyu/mystudyproject/MyStudyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenyu.mystudyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStudyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
