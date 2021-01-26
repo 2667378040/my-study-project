957099882163c41702cb80a1da8baf7a025400ab
chenyu <yu.chen@tioad.com>
2021-01-26 10:40:12 +0800

    git commit "1"

---
+++ src/main/java/com/chenyu/mystudyproject/a.java
@@ -0,0 +1,13 @@
+package com.chenyu.mystudyproject;
+
+/**
+ * @author : Chenyu
+ * @version : 0.0.0
+ * @time : 2021/1/26
+ */
+public class a {
+    public void a1(){
+        System.out.println(123);
+    }
+
+}
---
+++ src/main/java/com/chenyu/mystudyproject/b.java
@@ -0,0 +1,9 @@
+package com.chenyu.mystudyproject;
+
+/**
+ * @author : Chenyu
+ * @version : 0.0.0
+ * @time : 2021/1/26
+ */
+public class b {
+}
